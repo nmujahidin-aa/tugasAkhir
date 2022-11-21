792333b77dfd75c9b0020800f0e81d19e4a6af67
nmujahidin-aa <nurmujahidin18@gmail.com>
2022-11-21 14:55:57 +0700

    Login

---
+++ resources/views/Layout/Login/layoutHead.blade.php
@@ -0,0 +1,14 @@
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Login</title>
+
+<!-- Bootstrap -->
+<link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
+
+<!-- Font -->
+<link rel="stylesheet" href="assets/css/material-design-iconic-font.min.css">
+
+<!-- Main -->
+<link rel="stylesheet" href="assets/css/styleLogin.css">
+
---
+++ resources/views/Layout/Login/layoutScript.blade.php
@@ -0,0 +1,5 @@
+<!-- Jquery -->
+<script type="text/javascript" src="assets/js/jquery.min.js"></script>
+
+<!-- Bootstrap -->
+<script type="text/javascript" src="assets/js/bootstrap.bundle.min.js"></script>
