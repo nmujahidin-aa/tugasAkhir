67ecb4174c8e5233ededfbce1eabb0c4983ab2cc
safranst <firaanissa22@gmail.com>
2022-12-01 22:23:30 +0700

    bagian setelah login/Homepage+Footer

--- resources/views/Layout/LandingPage/layoutFooter.blade.php
+++ resources/views/Layout/LandingPage/layoutFooter.blade.php
@@ -1,58 +1,50 @@
 <!-- ======= Footer ======= -->
 <footer id="footer">
 
-  <div class="footer-top">
+<footer class="bg-light border-top pt-5 pb-2">
     <div class="container">
-      <div class="row">
-
-        <div class="col-lg-3 col-md-6 footer-contact">
-          <h3>Rumah Pustaka</h3>
+        <div class="row">
+            <div class="col-12 col-md-12 col-lg-4 text-center text-lg-start" style="margin-bottom: 40px;">
+                <img class="mb-2" src="image/logo.png" alt="" height="40">
+                <br>
+                <a style="margin-left: 3px; margin-right: 13px;" class="link-dark" target="_blank" href=""><i class="bx bxl-facebook fs-5"></i></a>
+                <a style="margin-right: 13px;" class="link-dark" target="_blank" href=""><i class="bx bxl-instagram fs-5"></i></a>
+                <a style="margin-right: 13px;" class="link-dark" target="_blank" href=""><i class="bx bxl-twitter fs-5"></i></a>
+                <br><br>
+                <small><i>Senin-Jum'at 09.00 - 17.00 WIB</i></small><br>
+                <small>Email: </small><a class="text-decoration-none link-dark" target="_blank" href=""><b>rumahpustaka@gmail.com</b></a>
+              </div>
+              <div class="col-6 col-md-3 col-lg-2">
+                  <p><small><b>PERUSAHAAN</b></small></p>
+                  <ul class="list-unstyled">
+                      <li><a class="text-decoration-none link-dark" href="">Tentang Rumah Pustaka</a></li>
+                      <li><a class="text-decoration-none link-dark" href="">Partnership</a></li>
+                  </ul>
+              </div>
+              <div class="col-6 col-md-3 col-lg-2">
+                  <p><small><b>PENYELENGGARA</b></small></p>
+                  <ul class="list-unstyled text-small">
+                      <li><a class="text-decoration-none link-dark" href="">Pendaftaran</a></li>
+                      <li><a class="text-decoration-none link-dark" href="">Biaya</a></li>
+                  </ul>
+              </div>
+              <div class="col-6 col-md-3 col-lg-2">
+                <p><small><b>BANTUAN</b></small></p>
+                  <ul class="list-unstyled text-small">
+                      <li><a class="text-decoration-none link-dark" href="">Hubungi Kami</a></li>
+                      <li><a class="text-decoration-none link-dark" href="">Status</a></li>
+                  </ul>
+              </div>
+              <div class="col-6 col-md-3 col-lg-2">
+                  <p><small><b>LEGAL</b></small></p>
+                  <ul class="list-unstyled text-small">
+                      <li><a class="text-decoration-none link-dark" href="">Ketentuan</a></li>
+                      <li><a class="text-decoration-none link-dark" href="">Kebijakan Privasi</a></li>
+                  </ul>
+              </div>
         </div>
-
-        <div class="col-lg-1 col-md-4 footer-links">
-          <ul>
-            <li><i class="bx bx-chevron-right"></i> <a href="#">Home</a></li>
-          </ul>
-        </div>
-
-        <div class="col-lg-1 col-md-4 footer-links">
-          <ul>
-            <li><i class="bx bx-chevron-right"></i> <a href="#">Services</a></li>
-          </ul>
-        </div>
-
-        <div class="col-lg-1 col-md-4 footer-links">
-          <ul>
-            <li><i class="bx bx-chevron-right"></i> <a href="#">About</a></li>
-          </ul>
-        </div>
-
-        <div class="col-lg-1 col-md-4 footer-links">
-          <ul>
-            <li><i class="bx bx-chevron-right"></i> <a href="#">Terms</a></li>
-          </ul>
-        </div>
-
-        <div class="col-lg-1 col-md-4 footer-links">
-          <ul>
-            <li><i class="bx bx-chevron-right"></i> <a href="#">PrivacyPolicy </a></li>
-          </ul>
-        </div>
-
-        <div class="col-lg-3 col-mx-5 justify-content-end footer-links">
-          <h4>Contact Us</h4>
-          <div class="social-links mt-3">
-            <a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
-            <a href="#" class="facebook"><i class="bx bxl-facebook"></i></a>
-            <a href="#" class="instagram"><i class="bx bxl-instagram"></i></a>
-            <a href="#" class="google-plus"><i class="bx bxl-github"></i></a>
-            <a href="#" class="linkedin"><i class="bx bx-envelope"></i></a>
-          </div>
-        </div>
-
-      </div>
     </div>
-  </div>
+</footer>
 
   <div class="container py-4">
     <div class="copyright">
