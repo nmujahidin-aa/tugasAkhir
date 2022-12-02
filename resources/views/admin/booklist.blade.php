9d1b85e7be9b5fb1ebf270891839dc31915f99db
Mashiroo21 <117349766+Mashiroo21@users.noreply.github.com>
2022-12-02 10:08:27 +0700

    ke enter

--- resources/views/admin/booklist.blade.php
+++ resources/views/admin/booklist.blade.php
@@ -4,7 +4,6 @@
     <h1 class="h3 mb-3">
         <strong>Buku</strong> Terdaftar
     </h1>
-
     <div class="row">
         <div class="col-12 col-lg-8 col-xxl-9 d-flex">
             <div class="card flex-fill">
