adfb77c539e1f327e4e2da1155df2bbf4179d86f
Mashiroo21 <117349766+Mashiroo21@users.noreply.github.com>
2022-12-06 11:04:15 +0700

    View Contact

--- resources/views/users/contact.blade.php
+++ resources/views/users/contact.blade.php
@@ -2,8 +2,23 @@
 @section('title', 'Contact')
 @section('content')
 
-<div class="container" style="margin-top: 65px; background: #e9ecef;">
-	<div>Contact Me</div>
+<div class="container" style="margin-top: 70px; background: #e9ecef; min-height:27rem">
+	<div class="row justify-content-center">
+		<div class="col-md-12 col-lg-8">
+			<div class="card">
+				<h5 class="card-header" style="text-align: center;">Our Contact</h5>
+				<div class="card-body">
+					<p class="card-text" style="text-align: center; margin-top: 20px;">Hubungi Kami melalui platform sosial media kami yang tersedia.</p>
+					<br>
+					<div class="col" style="text-align: center; margin-bottom: 20px;">
+						<a style="margin-left: 3px; margin-right: 13px;" class="link-dark" target="_blank" href=""><i class="bx bxl-facebook fs-5"></i></a>
+						<a style="margin-right: 13px;" class="link-dark" target="_blank" href=""><i class="bx bxl-instagram fs-5"></i></a>
+						<a style="margin-right: 13px;" class="link-dark" target="_blank" href=""><i class="bx bxl-twitter fs-5"></i></a>
+					</div>
+				</div>
+			  </div>
+		</div>
+	</div>
 </div>
 
 @endsection
