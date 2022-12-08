72bfb531ffbb94dff56ba11d695177986a200f2c
nmujahidin-aa <nurmujahidin18@gmail.com>
2022-12-08 18:08:11 +0700

    fitur download dan manggil data

--- app/Http/Controllers/User/HomeController.php
+++ app/Http/Controllers/User/HomeController.php
@@ -11,14 +11,35 @@ use Illuminate\Support\Facades\Log;
 use App\Enums\RoleEnum;
 use App\Enums\UserEnum;
 use App\Models\Book;
+use App\Models\Category;
 use App\Helpers\UploadHelper;
 use Auth;
 use Error;
 
 class HomeController extends Controller
 {
-    public function index()
+    public function __construct(){
+        $this->route = "home.";
+        $this->view = "users.homePage";
+        $this->book = new Book();
+        $this->category = new Category();
+    }
+    
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+
+    public function index(Request $request)
     {
-        return view('users.homePage');
+        $table = $this->book;
+        $table = $table->orderBy("created_at","DESC");
+        $table = $table->get();
+
+        $data = [
+            'table' => $table,
+        ];
+        return view($this->view,$data);
     }
 }
--- resources/views/users/homePage.blade.php
+++ resources/views/users/homePage.blade.php
@@ -6,35 +6,33 @@
 	<div style="margin-top: 65px;">
 		<div class="row">
 			<div class="col-lg-8 col-md-12">
-				<div class="card" style="margin-top: 10px; margin-bottom: 200px;">
+				@foreach($table as $index => $row)
+				<div class="card" style="margin-top: 10px;">
+					
 					<div class="card-body">
 						
 						<div>
-							<strong>Title Buku</strong>
+							<strong>{{$row->title}}</strong>
 						</div>
 
 						<div class="mt-2">
-							<span class="text-center " style="padding-bottom: 3px; padding-left: 10px; padding-right: 10px; max-width: 15%; background-color: #C5E8E5; font-size: 13px;">category buku</span>
-							<span class="text-muted" style="font-size: 13px;"><i class="bx bx-history"></i> 10 menit lalu</span>
+							<span class="text-center " style="padding-bottom: 3px; padding-left: 10px; padding-right: 10px; max-width: 15%; background-color: #C5E8E5; font-size: 13px;">{{$row->category->name ?? ""}}</span>
+							<span class="text-muted" style="font-size: 13px;"><i class="bx bx-history"></i>{{date('d-m-Y',strtotime($row->published_at))}}</span>
 						</div>
 
 						<div class="mt-4">
-							<p style="font-weight: bold;">Username</p>
+							<p style="font-weight: bold;">{{$row->author}}</p>
 							<span>Description:</span>
-							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-							tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-							quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-							consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-							cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-							proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+							<p>{{$row->description}}</p>
 						</div>
 
 						<div class="mt-2">
-							<div class="btn btn-sm btn-outline-success rounded-pill">Donwload</div>
+							<a href="{{ Storage::url($row->file) }}" download class="btn btn-sm btn-outline-success rounded-pill">Donwload</a>
 						</div>
 
 					</div>
 				</div>
+				@endforeach
 			</div>
 
 
